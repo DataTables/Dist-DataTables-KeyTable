f6a6c97e90eff3fa6ccefe1509f0bbac4ad74cb5
dtbuild <dtbuild@rigil>
2024-05-28 09:25:13 +0000

    707f086d5f77cf3e50eefb9e1157466b8f27d4d6 Fix: If inline editing with Editor and `escape` is pressed, when you blur focus away from a cell it would still submit.
    Sync to source repo @707f086d5f77cf3e50eefb9e1157466b8f27d4d6

--- js/dataTables.keyTable.mjs
+++ js/dataTables.keyTable.mjs
@@ -1,4 +1,4 @@
-/*! KeyTable 2.12.0
+/*! KeyTable 2.12.1
  * © SpryMedia Ltd - datatables.net/license
  */
 
@@ -12,7 +12,7 @@ let $ = jQuery;
 /**
  * @summary     KeyTable
  * @description Spreadsheet like keyboard navigation for DataTables
- * @version     2.12.0
+ * @version     2.12.1
  * @file        dataTables.keyTable.js
  * @author      SpryMedia Ltd
  * @contact     datatables.net
@@ -974,7 +974,7 @@ $.extend(KeyTable.prototype, {
 	_keyAction: function (action) {
 		var editor = this.c.editor;
 
-		if (editor && editor.mode()) {
+		if (editor && editor.mode() && editor.display()) {
 			editor.submit(action);
 		}
 		else {
@@ -1287,7 +1287,7 @@ KeyTable.defaults = {
 	tabIndex: null
 };
 
-KeyTable.version = '2.12.0';
+KeyTable.version = '2.12.1';
 
 $.fn.dataTable.KeyTable = KeyTable;
 $.fn.DataTable.KeyTable = KeyTable;
