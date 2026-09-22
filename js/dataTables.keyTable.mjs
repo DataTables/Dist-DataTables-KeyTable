3bf9141e6ae4775d5c1be1244be7d328dacf4ff7
dtbuild <dtbuild@rigil>
2026-09-22 10:40:13 +0000

    1297ad8540ebfaa842d9c5422a28d2de8f1ef309 Release 3.1.0
    Sync to source repo @1297ad8540ebfaa842d9c5422a28d2de8f1ef309

--- js/dataTables.keyTable.mjs
+++ js/dataTables.keyTable.mjs
@@ -1,4 +1,4 @@
-/*! KeyTable 3.0.0 for DataTables
+/*! KeyTable 3.1.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
@@ -1050,7 +1050,7 @@ KeyTable.defaults = {
      */
     tabIndex: null
 };
-KeyTable.version = '3.0.0';
+KeyTable.version = '3.1.0';
 
 
 DataTable.KeyTable = KeyTable;
